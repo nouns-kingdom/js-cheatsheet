5877c65fdce6ce966c0454c7009b45da0860e778
nouns-kingdom <71849804+nouns-kingdom@users.noreply.github.com>
2020-12-11 11:06:28 -0500

    Update array.js

--- array.js
+++ array.js
@@ -1,2 +1,11 @@
 let fruits = ['apple', 'banana'];
 console.log(fruits.length);
+
+// 2D array
+let array = [
+    [1, 2, 3],
+    [3, 4, 4],
+];
+
+console.log(array.length);
+console.log(array[0].length);
